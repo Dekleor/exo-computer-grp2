4a05ac2401dd048e58bbabe26486c324ac7f45bb
hardkos77 <62692670+hardkos77@users.noreply.github.com>
2021-03-02 16:54:29 +0100

    correction form choices

--- src/Form/FormComponentType.php
+++ src/Form/FormComponentType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Component;
 use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,7 +21,9 @@ class FormComponentType extends AbstractType
             ->add('price', MoneyType::class)
             ->add('type', TextType::class)
             ->add('brand', TextType::class)
-
+            ->add('type', ChoiceType::class, [
+                'choices' => array_flip(Component::AVAILABLE_TYPES),
+            ]);
         ;
     }
 
